f39a839faa6e6cdc248efee09359087dc6fad751
Mix2511 <47683500+Mix2511@users.noreply.github.com>
2020-04-15 09:01:17 +0500

    Initial commit

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,297 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.Button_h = new System.Windows.Forms.TextBox();
+            this.Button_K = new System.Windows.Forms.TextBox();
+            this.Button_A = new System.Windows.Forms.TextBox();
+            this.Button_Tn = new System.Windows.Forms.TextBox();
+            this.Button_Qn = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.Button_Tz = new System.Windows.Forms.TextBox();
+            this.Button_Qz = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.Button_th = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(40, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(369, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "задача 1 расчет фазовой траектории изменнения температуры слитка\r\n";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(41, 59);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(26, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Th=";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(41, 95);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(19, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "h=";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(40, 131);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(20, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "K=";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(219, 59);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(20, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "A=";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(219, 95);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(37, 13);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Tнач=";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(219, 131);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(27, 13);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Qн=";
+            // 
+            // Button_h
+            // 
+            this.Button_h.Location = new System.Drawing.Point(74, 92);
+            this.Button_h.Name = "Button_h";
+            this.Button_h.Size = new System.Drawing.Size(127, 20);
+            this.Button_h.TabIndex = 9;
+            // 
+            // Button_K
+            // 
+            this.Button_K.Location = new System.Drawing.Point(75, 128);
+            this.Button_K.Name = "Button_K";
+            this.Button_K.Size = new System.Drawing.Size(126, 20);
+            this.Button_K.TabIndex = 10;
+            // 
+            // Button_A
+            // 
+            this.Button_A.Location = new System.Drawing.Point(258, 56);
+            this.Button_A.Name = "Button_A";
+            this.Button_A.Size = new System.Drawing.Size(122, 20);
+            this.Button_A.TabIndex = 11;
+            // 
+            // Button_Tn
+            // 
+            this.Button_Tn.Location = new System.Drawing.Point(258, 92);
+            this.Button_Tn.Name = "Button_Tn";
+            this.Button_Tn.Size = new System.Drawing.Size(122, 20);
+            this.Button_Tn.TabIndex = 12;
+            // 
+            // Button_Qn
+            // 
+            this.Button_Qn.Location = new System.Drawing.Point(258, 128);
+            this.Button_Qn.Name = "Button_Qn";
+            this.Button_Qn.Size = new System.Drawing.Size(122, 20);
+            this.Button_Qn.TabIndex = 13;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(40, 194);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(424, 13);
+            this.label9.TabIndex = 14;
+            this.label9.Text = "задача 2 расчет соотношения коэффицентов k & A при заданом уровне нагрева Tз";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(41, 227);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(38, 13);
+            this.label10.TabIndex = 15;
+            this.label10.Text = "Tзад=";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(41, 263);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(27, 13);
+            this.label11.TabIndex = 16;
+            this.label11.Text = "Qн=";
+            // 
+            // Button_Tz
+            // 
+            this.Button_Tz.Location = new System.Drawing.Point(85, 227);
+            this.Button_Tz.Name = "Button_Tz";
+            this.Button_Tz.Size = new System.Drawing.Size(134, 20);
+            this.Button_Tz.TabIndex = 17;
+            // 
+            // Button_Qz
+            // 
+            this.Button_Qz.Location = new System.Drawing.Point(85, 263);
+            this.Button_Qz.Name = "Button_Qz";
+            this.Button_Qz.Size = new System.Drawing.Size(134, 20);
+            this.Button_Qz.TabIndex = 18;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(462, 14);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(335, 264);
+            this.listBox1.TabIndex = 19;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(44, 166);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(117, 19);
+            this.button1.TabIndex = 20;
+            this.button1.Text = "Рассчет задачи 1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(44, 307);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(117, 19);
+            this.button2.TabIndex = 21;
+            this.button2.Text = "Рассчет задачи 2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Button_th
+            // 
+            this.Button_th.Location = new System.Drawing.Point(75, 56);
+            this.Button_th.Name = "Button_th";
+            this.Button_th.Size = new System.Drawing.Size(126, 20);
+            this.Button_th.TabIndex = 22;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(818, 450);
+            this.Controls.Add(this.Button_th);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.Button_Qz);
+            this.Controls.Add(this.Button_Tz);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.Button_Qn);
+            this.Controls.Add(this.Button_Tn);
+            this.Controls.Add(this.Button_A);
+            this.Controls.Add(this.Button_K);
+            this.Controls.Add(this.Button_h);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox Button_h;
+        private System.Windows.Forms.TextBox Button_K;
+        private System.Windows.Forms.TextBox Button_A;
+        private System.Windows.Forms.TextBox Button_Tn;
+        private System.Windows.Forms.TextBox Button_Qn;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox Button_Tz;
+        private System.Windows.Forms.TextBox Button_Qz;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox Button_th;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            double tn, Tz, h, k, a, Qn, T, dT, i, tt;
+            string s;
+            tn = Convert.ToDouble(Button_Tn.Text);//время нагревания
+            h = Convert.ToDouble(Button_h.Text);//шаг замеров
+            k = Convert.ToDouble(Button_K.Text);//коэфф k
+            a = Convert.ToDouble(Button_A.Text);//коэфф A
+            T = Convert.ToDouble(Button_th.Text);//T нач
+            Qn = Convert.ToDouble(Button_Qn.Text);//Q нагревателя
+            i = 1;
+            tt = 0;
+            while (tn > tt)
+            {
+                s = "'точка №" + i.ToString() + "в момент времени t=" + tt.ToString() + " температура T=" + T.ToString() + ".";
+                listBox1.Items.Add(s);
+                dT = k * (Qn - a * T * T);
+                T = T + dT * h;
+                tt += h;
+                i++;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double tz, qz,a;
+            string s;
+            tz = Convert.ToDouble(Button_Tz.Text);//T заданная
+            qz = Convert.ToDouble(Button_Qz.Text);//q нагревателя
+            a = qz / (tz * tz);
+            s = "'требуемый уровень нагрева Tзад=" + tz.ToString() + "может быть достигнут при a=<" + a.ToString() + "при любом k";
+            MessageBox.Show(s);
+        }
+    }
+}
